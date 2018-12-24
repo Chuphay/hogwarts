f4d29a954d67afa35a92e15cf3091c831fbea5d5
chuphay <chuphay@gmail.com>
2018-12-24 00:10:59 +0000

    getting better

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 secret_password="CHANGE THIS"
 
+sudo rmdir /magical_world
+sudo mkdir /magical_world
 sudo userdel dumbledore
-sudo useradd -m -s /bin/bash dumbledore
+sudo useradd -m -d /magical_world/headmasters_office -s /bin/bash dumbledore
 echo dumbledore:"$secret_password" | chpasswd
 sudo delgroup teachers
 sudo groupadd teachers
 sudo usermod -a -G teachers dumbledore
-sudo rmdir /magical_world
-sudo mkdir /magical_world
+# sudo usermod -g teachers dumbledore
 sudo chown dumbledore:teachers /magical_world
