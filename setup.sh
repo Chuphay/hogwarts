43f24abc53909a97e942092cb8db4f06f47cbded
chuphay <chuphay@gmail.com>
2018-12-24 00:29:52 +0000

    slowly making a world

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@
 
 secret_password="CHANGE THIS"
 
-sudo rmdir /magical_world
-sudo mkdir /magical_world
-sudo userdel dumbledore
-sudo useradd -m -d /magical_world/headmasters_office -s /bin/bash dumbledore
+rm -rf /magical_world
+mkdir /magical_world
+mkdir /magical_world/hogwarts
+userdel dumbledore
+useradd -m -d /magical_world/hogwarts/headmasters_office -s /bin/bash dumbledore
 echo dumbledore:"$secret_password" | chpasswd
-sudo delgroup teachers
-sudo groupadd teachers
-sudo usermod -a -G teachers dumbledore
-# sudo usermod -g teachers dumbledore
-sudo chown dumbledore:teachers /magical_world
+delgroup teachers
+groupadd teachers
+usermod -a -G teachers dumbledore
+cd /magical_world
+mkdir forbidden_forest hagrids_hut hogsmeade lake train_station
+chown -R dumbledore:teachers /magical_world
